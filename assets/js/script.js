5924e6fa0814d98689457bfa522eca8e10ae4b68
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2021-11-25 20:02:03 -0300

    primer desafio hecho

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//primer desafio
+// setTimeout(() => {
+//     async function albumAsincrono() {
+//     try {
+//         let contenido= await fetch('https://jsonplaceholder.typicode.com/photos')
+//         let response = await contenido.json()
+//         let iterar = response.filter((e) => e.id <= 20)
+//         console.log(iterar)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// albumAsincrono() 
+// }, 3000);
+
